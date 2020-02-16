a72c0af6a3f76c3baa6e4f736d9666c1e48750de
xudaye2001 <git@github.com:xudaye2001/security-in-action.git>
2020-02-16 18:02:02 +0800

    test

---
+++ src/main/java/com/waylau/spring/boot/blog/controller/AdminController.java
@@ -0,0 +1,18 @@
+package com.waylau.spring.boot.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/admins")
+public class AdminController {
+
+    @GetMapping
+    public ModelAndView listUsers(Model model) {
+        model.addAttribute("menulist",model);
+        return new ModelAndView("admins/index");
+    }
+}
---
+++ src/main/java/com/waylau/spring/boot/blog/controller/BlogController.java
@@ -0,0 +1,15 @@
+package com.waylau.spring.boot.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/bologs")
+public class BlogController {
+    public String bologs(@RequestParam(value = "order", required = false, defaultValue = "new") String order,
+                         @RequestParam(value = "keyword", required = false, defaultValue = "") String keyword) {
+        System.out.println("order:"+order+"keyword:"+keyword);
+        return "redirect:/index?order="+order+"&keyword="+keyword;
+    }
+}
